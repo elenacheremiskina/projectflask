1c3759962768efb2ccb8064b4ab13d2112cf8cad
elenacheremiskina <43518487+elenacheremiskina@users.noreply.github.com>
2020-05-18 18:26:42 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request, g, url_for
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.exceptions import HTTPException
-from werkzeug.exceptions import default_exceptions
-import settings
+from werkzeug.exceptions import HTTPException, default_exceptions
+import urllib
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
-
 
 @app.errorhandler(Exception)
 def handle_error(e):
@@ -18,16 +17,17 @@ def handle_error(e):
 for ex in default_exceptions:
     app.register_error_handler(ex, handle_error)
 
-import urllib
+
 params = urllib.parse.quote_plus('DRIVER={SQL Server};SERVER=LAPTOP-JM1M6JLK\SQLEXPRESS;DATABASE=Analysis;Trusted_Connection=yes;')
 app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
-app.config['BUNDLE_ERRORS'] = settings.BUNDLE_ERRORS
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['BUNDLE_ERRORS'] = True
 
 db = SQLAlchemy(app)
 api = Api(app)
 api.prefix = '/api'
-
+auth = HTTPBasicAuth()
 
 from endpoints.users.resource import UsersResource
 from endpoints.text.resource import TextResource
@@ -37,6 +37,18 @@ from endpoints.freq.resource import FreqResource
 from endpoints.register.resource import RegisterResource
 
 api.add_resource(UsersResource, '/', '/<int:id_user>')
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, %s!' % g.user.email})
+
 api.add_resource(TextResource, '/text', '/text/<int:id_text>')
 api.add_resource(SyntaxResource, '/syntax', '/syntax/<int:id_result>')
 api.add_resource(SemanticResource, '/semantic', '/semantic/<int:id_result>')
@@ -44,4 +56,4 @@ api.add_resource(FreqResource, '/freq', '/freq/<int:id_result>')
 api.add_resource(RegisterResource, '/register', '/register/<int:id_text>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
